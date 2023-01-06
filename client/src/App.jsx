14e21fdd02368e7d61daded2b4b8083a6f9ceff8
heysourin <sourin07@gmail.com>
2023-01-06 23:52:49 +0530

    Tailwind updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  return <div className="relative sm:p-8 p-4 bg-[#13131a] flex flex-row ">
-    App
-  </div>
+  return (
+    <div className="relative sm:p-8 p-4 bg-[#13131a] flex flex-row ">
+
+      <div className="sm:flex hidden mr-10 relative font-bold">Sidebar</div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280-px] mx-auto sm:pr-5 ">
+        Navbar
+        <p className='font-xl font-bold'>Text</p>
+      </div>
+    </div>
+  )
 }
 
 export default App
