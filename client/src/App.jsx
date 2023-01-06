b2f00d6ff560ec01266a3e8c20fae396012710a3
heysourin <sourin07@gmail.com>
2023-01-06 23:17:06 +0530

    Contract updated

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return <div className="relative sm:p-8 p-4 bg-[#13131a] flex flex-row ">
+    App
+  </div>
+}
+
+export default App
